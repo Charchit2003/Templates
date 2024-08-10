f9e70262f452f39f860c2a63e100bcb30001254f
Charchit Paharia <95647063+Charchit2003@users.noreply.github.com>
2024-08-10 11:21:09 +0530

    Create Strings.cpp
    Important alogs for strings

---
+++ Strings.cpp
@@ -0,0 +1,108 @@
+int get_hash(string const& s) {
+    const int p = 31;
+    const int m = 1e9 + 9;
+    int hash_value = 0;
+    int p_pow = 1;
+    for (char c : s) {
+        hash_value = (hash_value + (c - 'a' + 1) * p_pow) % m;
+        p_pow = (p_pow * p) % m;
+    }
+    return hash_value;
+}
+
+
+// Manacher's algorithm is a string-matching algorithm that is used to find the longest palindromic substring in a given string.
+// Time Complexity is O(N)!!
+vector<int> manacher_odd(string s) {
+    int n = s.size();
+    s = "$" + s + "^";
+    vector<int> p(n + 2);
+    int l = 1, r = 1;
+    for(int i = 1; i <= n; i++) {
+        p[i] = max(0LL, min(r - i, p[l + (r - i)]));
+        while(s[i - p[i]] == s[i + p[i]]) {
+            p[i]++;
+        }
+        if(i + p[i] > r) {
+            l = i - p[i], r = i + p[i];
+        }
+    }
+    return vector<int>(begin(p) + 1, end(p) - 1);
+}
+
+vector<int> manacher(string s) {
+    string t;
+    for(auto c: s) {
+        t += string("#") + c;
+    }
+    auto res = manacher_odd(t + "#");
+    return vector<int>(begin(res) + 1, end(res) - 1);
+}
+
+
+
+// Rabin Karp Alog is used to compare two strings in O(n+m) with forward polynomial hashing.
+// Given two strings - a pattern  S  and a text T , determine if the pattern appears in the text and if it does, enumerate all its occurrences.
+vector<int> rabin_karp(string const& s, string const& t) {
+    const int p = 31; 
+    const int m = 1e9 + 9;
+    int S = s.size(), T = t.size();
+
+    vector<long long> p_pow(max(S, T)); 
+    p_pow[0] = 1; 
+    for (int i = 1; i < (int)p_pow.size(); i++) 
+        p_pow[i] = (p_pow[i-1] * p) % m;
+
+    vector<long long> h(T + 1, 0); 
+    for (int i = 0; i < T; i++)
+        h[i+1] = (h[i] + (t[i] - 'a' + 1) * p_pow[i]) % m; 
+    long long h_s = 0; 
+    for (int i = 0; i < S; i++) 
+        h_s = (h_s + (s[i] - 'a' + 1) * p_pow[i]) % m; 
+
+    vector<int> occurrences;
+    for (int i = 0; i + S - 1 < T; i++) {
+        long long cur_h = (h[i+S] + m - h[i]) % m;
+        if (cur_h == h_s * p_pow[i] % m)
+            occurrences.push_back(i);
+    }
+    return occurrences;
+}
+
+
+
+// zfunction (O(N))-> ith element is equal to greatest number of char starting from pos i that coincide with 1st char of s.
+vector<int> z_function(string s) {
+    int n = s.size();
+    vector<int> z(n);
+    int l = 0, r = 0;
+    for(int i = 1; i < n; i++) {
+        if(i < r) {
+            z[i] = min(r - i, z[i - l]);
+        }
+        while(i + z[i] < n && s[z[i]] == s[i + z[i]]) {
+            z[i]++;
+        }
+        if(i + z[i] > r) {
+            l = i;
+            r = i + z[i];
+        }
+    }
+    return z;
+}
+
+
+
+
+
+void solve(){
+
+    string s; cin >> s;
+    auto v = manacher(s);
+    for (auto &x : v) x--;
+
+    // Now v[l+r] gives the largest palindromic sequence from indexs l to r included.
+    // if(v[l+r]==r-l+1) it means substring from l to r is palindromic sequence
+
+    return;
+}
