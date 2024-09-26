bc79c05ec58e48bac2de30f06d8f1761649591c0
Charchit Paharia <95647063+Charchit2003@users.noreply.github.com>
2024-09-26 22:46:36 +0530

    Tarjan's Algorithm

---
+++ Bridges_and_Articulation_Points.cpp
@@ -0,0 +1,113 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+private:
+    int timer = 1;
+    void dfsbridges(int node, int parent, vector<int> &vis,
+        vector<int> adj[], int tin[], int low[], vector<vector<int>> &bridges) {
+        vis[node] = 1;
+        tin[node] = low[node] = timer;
+        timer++;
+        for (auto it : adj[node]) {
+            if (it == parent) continue;
+            if (vis[it] == 0) {
+                dfsbridges(it, node, vis, adj, tin, low, bridges);
+                low[node] = min(low[it], low[node]);
+                // node --- it
+                if (low[it] > tin[node]) {
+                    bridges.push_back({it, node});
+                }
+            }
+            else {
+                low[node] = min(low[node], low[it]);
+            }
+        }
+    }
+
+    int timer = 1;
+    void dfsart(int node, int parent, vector<int> &vis, int tin[], int low[],
+        vector<int> &mark, vector<int>adj[]) {
+        vis[node] = 1;
+        tin[node] = low[node] = timer;
+        timer++;
+        int child = 0;
+        for (auto it : adj[node]) {
+            if (it == parent) continue;
+            if (!vis[it]) {
+                dfsart(it, node, vis, tin, low, mark, adj);
+                low[node] = min(low[node], low[it]);
+                if (low[it] >= tin[node] && parent != -1) {
+                    mark[node] = 1;
+                }
+                child++;
+            }
+            else {
+                low[node] = min(low[node], tin[it]);
+            }
+        }
+        if (child > 1 && parent == -1) {
+            mark[node] = 1;
+        }
+    }
+
+public:
+    vector<vector<int>> criticalConnections(int n,
+    vector<vector<int>>& connections) {
+        vector<int> adj[n];
+        for (auto it : connections) {
+            int u = it[0], v = it[1];
+            adj[u].push_back(v);
+            adj[v].push_back(u);
+        }
+        vector<int> vis(n, 0);
+        int tin[n];
+        int low[n];
+        vector<vector<int>> bridges;
+        dfsbridges(0, -1, vis, adj, tin, low, bridges);
+        return bridges;
+    }
+
+    vector<int> articulationPoints(int n, vector<int>adj[]) {
+        vector<int> vis(n, 0);
+        int tin[n];
+        int low[n];
+        vector<int> mark(n, 0);
+        for (int i = 0; i < n; i++) {
+            if (!vis[i]) {
+                dfsart(i, -1, vis, tin, low, mark, adj);
+            }
+        }
+        vector<int> ans;
+        for (int i = 0; i < n; i++) {
+            if (mark[i] == 1) {
+                ans.push_back(i);
+            }
+        }
+        if (ans.size() == 0) return { -1};
+        return ans;
+    }
+};
+
+int main() {
+
+    int n = 4;
+    vector<vector<int>> connections = {
+        {0, 1}, {1, 2},
+        {2, 0}, {1, 3}
+    };
+
+    Solution obj;
+    vector<vector<int>> bridges = obj.criticalConnections(n, connections);
+    for (auto it : bridges) {
+        cout << "[" << it[0] << ", " << it[1] << "] ";
+    }
+  
+    vector<int> nodes = obj.articulationPoints(n, adj);
+    for (auto node : nodes) {
+        cout << node << " ";
+    }
+    cout << endl;
+    cout << endl;
+    return 0;
+}
